573fe7d2919538ec32212250f609012b3375fff5
lishem12 <97026458@qq.com>
2021-03-03 17:19:09 +0800

    数组排序比较器

---
+++ src/main/java/basic_class_01/Code_00_BubbleSort.java
@@ -0,0 +1,47 @@
+package basic_class_01;
+
+import utils.SortComparator;
+
+/**
+ * 	冒泡排序
+ */
+public class Code_00_BubbleSort {
+
+	public static void bubbleSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		for (int i = 0; i < arr.length; ++i) {
+			for (int j = 1; j < arr.length - i; ++j) {
+				if (arr[j] < arr[j - 1]) {
+					swap(arr, j - 1, j);
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+
+	public static void main(String[] args) {
+		int maxSize = 20;
+		int maxValue = 200;
+		int testTime = 500000;
+		boolean succeed = true;
+		for (int i = 0; i < testTime; ++i) {
+			int[] arr1 = SortComparator.generateRandomArray(maxSize, maxValue);
+			int[] arr2 = SortComparator.copyArray(arr1);
+			bubbleSort(arr1);
+			SortComparator.comparator(arr2);
+			if (!SortComparator.isEqual(arr1, arr2)) {
+				succeed = false;
+				break;
+			}
+		}
+		System.out.println(succeed ? "NICE" : "FUCK");
+	}
+
+}
---
+++ src/main/java/utils/SortComparator.java
@@ -0,0 +1,48 @@
+package utils;
+
+import java.util.Arrays;
+
+public class SortComparator {
+
+	public static int[] generateRandomArray(int maxSize, int maxValue) {
+		int size = (int) (maxSize * Math.random());
+		int[] arr = new int[size];
+		for (int i = 0; i < size; ++i) {
+			arr[i] = (int) (Math.random() * (maxValue + 1) - Math.random() * maxValue);
+		}
+		return arr;
+	}
+
+	public static void comparator(int[] arr) {
+		Arrays.sort(arr);
+	}
+
+	public static boolean isEqual(int[] arr1, int[] arr2) {
+		if (arr1 == null && arr2 == null) {
+			return true;
+		}
+		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+			return false;
+		}
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; ++i) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int[] copyArray(int[] arr) {
+		if (arr == null) {
+			return null;
+		}
+		int[] arr2 = new int[arr.length];
+		for (int i = 0; i < arr.length; ++i) {
+			arr2[i] = arr[i];
+		}
+		return arr2;
+	}
+}
